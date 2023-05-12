de4cb1f78bba0928ca704dfb68e64116eae2960e
Orjinta Anthony <orjintaxavier@gmail.com>
2023-05-12 21:27:38 +0000

    db

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+
+app.use(express.json({ extended: false}));
+
 app.get('/', (req, res) => res.json({msg: 'Welcome to the Contact Book API'}));
 
 // Define Routes
@@ -15,4 +19,4 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT,  () => console.log(`Server started on port ${PORT}`));
