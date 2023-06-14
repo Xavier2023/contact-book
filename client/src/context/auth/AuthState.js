4a27d36d755c8d744af7fc28949b517d07356b5d
Orjinta Anthony <orjintaxavier@gmail.com>
2023-06-14 23:43:51 +0000

    new

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, {useReducer} from 'react';
 import axios from 'axios'
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -25,7 +26,23 @@ const AuthState = props => {
     const [state, dispatch] = useReducer(authReducer, initialState);
 
     // LOAD User
-    const loadUser = () => console.log();
+    const loadUser = async () => {
+        if (localStorage.token) {
+            setAuthToken(localStorage.token)
+        }
+
+        try {
+            const res = await axios.get('/api/auth');
+
+            dispatch({
+                type: USER_LOADED,
+                payload: res.data
+            
+            });
+        } catch (err) {
+            dispatch({type: AUTH_ERROR});
+        }
+    };
 
 
     // Register User
@@ -43,11 +60,13 @@ const AuthState = props => {
                 type: REGISTER_SUCCESS,
                 payload: res.data
             })
+          
+          loadUser();
         } catch (err) {
             dispatch({
                 type: REGISTER_FAIL,
                 payload: err.response.data.msg
-            })
+            });
         }
     }
 
