ae9a86c0332817fb45dd83721fc6c0e68e5559f6
Anthony <orjintaxavier@gmail.com>
2023-09-04 21:56:29 +0100

    new

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -4,26 +4,39 @@ import axios from 'axios';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
+    GET_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
+    CLEAR_CONTACTS,
     CLEAR_FILTER,
     CONTACT_ERROR
 } from '../types';
 
 const ContactState = props => {
     const initialState = {
-        contacts:[],
+        contacts: null,
         current: null,
         filtered: null,
         error: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
+    // Get Contacts
+    const getContacts = async () => {
 
+        try {
+            const res = await axios.get('/api/contacts')
+
+            dispatch({ type:GET_CONTACTS, payload: res.data})
+        } catch (err) {
+            dispatch({ type: CONTACT_ERROR, payload: err.response.msg})
+        }
+       
+    }
     // Add Contact
     const addContact = async contact => {
         const config = {
@@ -47,6 +60,12 @@ const ContactState = props => {
         dispatch({ type:DELETE_CONTACT, payload: id})
     }
 
+    //Clear Contacts 
+    const clearContacts = () => {
+        dispatch({ type:CLEAR_CONTACTS })
+    }
+
+
     // Set Current
     const setCurrent = contact => {
         dispatch({ type:SET_CURRENT, payload: contact})
@@ -66,7 +85,8 @@ const ContactState = props => {
     // Filter Contact
     const filterContacts = text => {
         dispatch({ type:FILTER_CONTACTS, payload: text })
-    }    
+    } 
+       
 
     // Clear Filter
     const clearFilter = () => {
@@ -79,8 +99,10 @@ const ContactState = props => {
             current: state.current,
             filtered: state.filtered,
             error: state.error,
+            getContacts,
             addContact,
             deleteContact,
+            clearContacts,
             setCurrent,
             clearCurrent,
             filterContacts,
